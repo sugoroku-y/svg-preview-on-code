ba62bf4fd620fae6965c41d1b9a1e3ccf37faae6
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-12-07 02:06:35 +0900

    style: 命名規則のルールを追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,10 +31,43 @@ export default tseslint.config(
 
     rules: {
       '@typescript-eslint/naming-convention': [
-        'warn',
+        'error',
+        // 省略したときのデフォルト設定
+        // ./node_modules/@typescript-eslint/eslint-plugin/dist/rules/naming-convention.js:11 より
         {
-          selector: 'import',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+          selector: 'default',
+          trailingUnderscore: 'allow',
+        },
+        {
           format: ['camelCase', 'PascalCase'],
+          selector: 'import',
+        },
+        {
+          format: ['camelCase', 'UPPER_CASE'],
+          leadingUnderscore: 'allow',
+          selector: 'variable',
+          trailingUnderscore: 'allow',
+        },
+        {
+          format: ['PascalCase'],
+          selector: 'typeLike',
+        },
+        // 以降はカスタマイズ設定
+        {
+          // クォートする必要のあるプロパティは命名規則から除外
+          selector: ['objectLiteralProperty', 'typeProperty'],
+          modifiers: ['requiresQuotes'],
+          format: null,
+        },
+        {
+          // クラスの静的で参照専用のプロパティはPascalCaseもしくはUPPER_CASE
+          selector: ['classProperty'],
+          format: ['PascalCase', 'UPPER_CASE'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'allow',
+          modifiers: ['readonly', 'static'],
         },
       ],
       '@typescript-eslint/no-unused-vars': [
